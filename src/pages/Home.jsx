6f1a36cf3e92f7ece33d2f2e78c5173c9b5a7950
juanson88 <hoangjohnson88@yahoo.com>
2026-07-29 00:14:35 -0500

    skeleton in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
+import SkeletonLoader from "../components/SkeletonLoader";
 
 const Home = ({ searchResults }) => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-const fetchDefaultMovies = async () => {
+    const fetchDefaultMovies = async () => {
+      setLoading(true);
       try {
-        if(!searchResults){
-        const { data } = await axios.get(
-          "https://www.omdbapi.com/?apikey=88b32aac&s=friends",
-        );
-
-        setMovies(data.Search || []);
-      }
+        if (!searchResults) {
+          const { data } = await axios.get(
+            "https://www.omdbapi.com/?apikey=88b32aac&s=friends"
+          );
+          setMovies(data.Search || []);
+        }
       } catch (error) {
         console.error("Error fetching default movies:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchDefaultMovies();
-  }, []);
+  }, [searchResults]);
 
   useEffect(() => {
     if (searchResults !== undefined) {
@@ -31,7 +35,13 @@ const fetchDefaultMovies = async () => {
 
   return (
     <div className="movie-list">
-      {movies.length > 0 ? (
+      {loading ? (
+        <>
+          <SkeletonLoader />
+          <SkeletonLoader />
+          <SkeletonLoader />
+        </>
+      ) : movies.length > 0 ? (
         movies.map((movie) => <MovieCard key={movie.imdbID} movie={movie} />)
       ) : (
         <h2>No movies found.</h2>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,23 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
-import SkeletonLoader from "../components/SkeletonLoader";
 
 const Movies = () => {
   const { title } = useParams();
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchMovies(movieTitle) {
-    setIsLoading(true);
-    try {
-      const { data } = await axios.get(`https://www.omdbapi.com/?apikey=88b32aac&s=${movieTitle}`);
-      setMovies(data.Search || []);
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-    } finally {
-      setIsLoading(false);
-    }
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?apikey=88b32aac&s=${movieTitle}`
+    );
+    setMovies(data.Search || []);
   }
 
   useEffect(() => {
@@ -26,18 +19,10 @@ const Movies = () => {
   }, [title]);
 
   return (
-    <div className="movies-container">
-      {isLoading ? (
-        <>
-          <SkeletonLoader />
-          <SkeletonLoader />
-          <SkeletonLoader />
-          <SkeletonLoader />
-          <SkeletonLoader />
-        </>
-      ) : (
-        movies.map((movie) => <MovieCard key={movie.imdbID} movie={movie} />)
-      )}
+    <div className="movie-list">
+      {movies.map((movie) => (
+        <MovieCard key={movie.imdbID} movie={movie} />
+      ))}
     </div>
   );
 };
