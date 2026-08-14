0e320b1e201141abfac90150d154271d793a6b9d
juanson88 <hoangjohnson88@yahoo.com>
2026-08-13 22:35:27 -0500

    ready for sub 4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <div className='footer-container'>
         <div className='footer-links'>
-        <a href="">About</a>
-        <a href="">Contact</a>
-        <a href="">Socials</a>
-        <a href="">Blog</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Socials</a>
+        <a href="#">Blog</a>
         </div>
         <div className='copyright'>
             <p>&copy; "Movie Search Database" All rights reserved</p>
